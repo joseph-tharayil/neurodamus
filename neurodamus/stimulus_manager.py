5e79665b6d568f5dff868c92ad593a995823a199
joe <tharayil@itis.swiss>
2026-02-05 13:00:36 +0100

    Revert "Axon sensitivity"
    This reverts commit 3c8ea2251304c474f8b735d7fd700e7369d4edcf.

--- neurodamus/stimulus_manager.py
+++ neurodamus/stimulus_manager.py
@@ -989,11 +989,11 @@ class SpatiallyUniformEField(BaseStim):
         if axon_index > 1:
             raise ValueError("More than 2 axon sections exist!")
         xpos = [soma_position[0], soma_position[0]]
-        zpos = [
-            soma_position[2] - 30 * int(axon_index),
-            soma_position[2] - 30 * int(axon_index + 1),
+        ypos = [
+            soma_position[1] - 30 * int(axon_index),
+            soma_position[1] - 30 * int(axon_index + 1),
         ]
-        ypos = [soma_position[1], soma_position[1]]
+        zpos = [soma_position[2], soma_position[2]]
         lens = [0, 1]
 
         # Interpolate the coordinates for the given location x along the segment
@@ -1013,11 +1013,11 @@ class SpatiallyUniformEField(BaseStim):
         if myelin_index > 0:
             raise ValueError("More than 1 myelin section exist!")
         xpos = [soma_position[0], soma_position[0]]
-        zpos = [
-            (soma_position[2] - 60) - 1000 * int(myelin_index),
-            (soma_position[2] - 60) - 1000 * int(myelin_index + 1),
+        ypos = [
+            (soma_position[1] - 60) - 1000 * int(myelin_index),
+            (soma_position[1] - 60) - 1000 * int(myelin_index + 1),
         ]
-        ypos = [soma_position[1], soma_position[1]]
+        zpos = [soma_position[2], soma_position[2]]
         lens = [0, 1]
 
         # Interpolate the coordinates for the given location x along the segment
