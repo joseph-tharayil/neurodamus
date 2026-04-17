d25668820bbe1bc8ed7906f0bd3e29c3c297be32
tharayil <tharayil@itis.swiss>
2026-04-17 12:43:06 +0000

    Updates default stimulus dt

--- neurodamus/stimulus_manager.py
+++ neurodamus/stimulus_manager.py
@@ -873,7 +873,7 @@ class SpatiallyUniformEField(BaseStim):
                 fields=self.fields,
                 ramp_up_time=self.ramp_up_time,
                 ramp_down_time=self.ramp_down_time,
-                dt=self.dt,
+                dt=0.5,
             )
             gid = target_point_list.gid
             if gid in self.stimList:
